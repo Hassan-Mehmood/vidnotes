372c22768e9873570e3d3573b2efb82a5708c6a4
Hassan-Mehmood <hassanafridi1913@gmail.com>
2025-08-22 14:56:25 +0500

    Working on user subscribing to channels

--- components/home/channel-card.tsx
+++ components/home/channel-card.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { ChannelItem } from '@/types/youtube';
+import Link from 'next/link';
 
 interface ChannelCardProps {
     channel: ChannelItem;
@@ -38,8 +39,8 @@ export default function ChannelCard({ channel }: ChannelCardProps) {
                 </p>
 
                 <div className="mt-auto space-y-2">
-                    <a
-                        href={`https://www.youtube.com/channel/${channel.id}?sub_confirmation=1`}
+                    <Link
+                        href={``}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-4 rounded-lg transition-colors duration-200 flex items-center justify-center group"
@@ -52,7 +53,7 @@ export default function ChannelCard({ channel }: ChannelCardProps) {
                             <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                         </svg>
                         Subscribe
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
